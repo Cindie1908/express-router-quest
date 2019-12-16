459b1528be02a392c6810c438c5f266d0009fd25
wilder <cindie.jouvin@gmail.com>
2019-12-16 13:57:41 +0100

    add new route tags

---
+++ routes/tags.js
@@ -0,0 +1,12 @@
+// routes/tags.js
+const express = require('express');
+
+const fakeTags = require('../data/tags');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json(fakeTags);
+  });
+
+module.exports = router;
